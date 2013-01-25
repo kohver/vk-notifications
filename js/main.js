c9103f6f55ba6358b7c742594adf9cac3800b996
kohver <m.arko@yandex.ru>
2013-01-25 19:14:05 +0400

    Описание, возможность отправлять уведомления от случайного пользователя

--- js/main.js
+++ js/main.js
@@ -1,41 +1,127 @@
-var textarea = ge('textarea');
+/**
+ * Модель всех пользователей
+ * @param options
+ * @constructor
+ */
+var User = function(options) {
+    var t = this;
+    t.id = options.id + '';
+    t.name = options.name + '';
+    t.photo = options.photo + '';
+};
 
-textarea.focus();
-addEvent('send-button', 'click', function() {
-    var text = trim(textarea.value);
-    if (text) {
-        textarea.value = '';
-        new Notification(1, Notification.MESSAGE, text);
-        textarea.focus();
-    } else {
-        textarea.focus();
+/**
+ * Модель всех сообщений
+ * @param options
+ * @constructor
+ */
+var Message = function(options) {
+    var t = this;
+
+    if (typeof options != 'object') {
+        throw new TypeError('Invalid options');
     }
-});
 
-addEvent('like-button', 'click', function() {
-    new Notification(1, Notification.LIKE);
-});
+    if (!(options.user instanceof User)) {
+        throw new TypeError('Invalid user');
+    }
 
-var Notification = function(user, type, message) {
+    t.text = options.text + '';
+    t.user = options.user;
+};
+
+(function() {
+    window.viewer = new User({
+        id: '1',
+        name: 'Павел Дуров',
+        photo: 'http://cs7003.userapi.com/v7003815/22a1/xgG9fb-IJ3Y.jpg'
+    });
+    var defaultUser = new User({
+        id: '4718705',
+        name: 'Артём Кохвер',
+        photo: 'http://cs10308.userapi.com/u4718705/e_be62b8f2.jpg'
+    });
+
+    var lastMessageNotification;
+    var user;
+    var textarea = ge('textarea');
+    var checkbox = ge('checkbox');
+    textarea.focus();
+
+    addEvent(textarea, 'keyup', function(e) {
+        if (e.keyCode == KEY.ENTER && (e.ctrlKey || e.metaKey)) {
+            sendNotification();
+        }
+    });
+    addEvent('send-button', 'click', function() {
+        sendNotification();
+    });
+    addEvent('like-button', 'click', function() {
+        new Notification(Notification.LIKE, {
+            user: defaultUser
+        });
+    });
+
+    function sendNotification() {
+        var text = trim(textarea.value);
+        var isNewUser = !!checkbox.checked;
+        if (isNewUser) {
+            user = new User({
+                id: Math.random(),
+                name: 'Случайный Пользователь',
+                photo: 'http://vk.com/images/camera_c.gif'
+            });
+        } else {
+            user = defaultUser;
+        }
+
+        if (text) {
+            var message = new Message({
+                user: user,
+                text: text
+            });
+            textarea.value = '';
+            if (lastMessageNotification &&
+                !lastMessageNotification.isHidden &&
+                message.user.id == lastMessageNotification.options.message.user.id) {
+                lastMessageNotification.addMessage(message);
+                lastMessageNotification.clearHideTimeout();
+                lastMessageNotification.setHideTimeout();
+            } else {
+                lastMessageNotification = new Notification(Notification.MESSAGE, {
+                    message: message
+                });
+            }
+            textarea.focus();
+        } else {
+            textarea.focus();
+        }
+    }
+})();
+
+/**
+ * Класс оповещений
+ * @param type
+ * @constructor
+ */
+var Notification = function(type) {
     var t = this;
 
     t.id = ++Notification.lastId;
     t.type = type;
-    t.user = user;
-    t.message = message;
+    t.options = arguments[1] || {};
     t.isHidden = true;
+    t.el = ce('div');
+    t.el.className = Notification.className;
+    t.el.style.zIndex = t.id;
 
     Notification.collection[t.id] = t;
     t.show();
-    return t;
 };
 
 Notification.prototype.show = function() {
     var t = this;
     t.isHidden = false;
-    t.el = ce('div');
-    t.el.className = Notification.className;
-    t.el.style.zIndex = t.id;
 
     Notification.el.appendChild(t.el);
 
@@ -58,21 +144,26 @@ Notification.prototype.show = function() {
 
 Notification.prototype.makeMessageNotification = function() {
     var t = this;
-    t.isFocused = false;
+    var message = t.options.message;
+
+    if (!(message instanceof Message)) {
+        throw new TypeError('Invalid message');
+    }
+
     t.el.innerHTML =
         '<div class="history">' +
             '<div class="message">' +
-                '<div class="photo">' +
-                    '<img src="http://cs10308.userapi.com/u4718705/e_be62b8f2.jpg" />' +
+                '<div class="photo" title="' + message.user.name + '">' +
+                    '<img src="' + message.user.photo + '" />' +
                 '</div>' +
                 '<div class="text">' +
-                    t.message.replace(/\n/g, '<br>') +
+                    message.text.replace(/\n/g, '<br>') +
                 '</div>' +
             '</div>' +
         '</div>' +
         '<div class="textarea-bg">' +
-            '<div class="photo">' +
-                '<img src="http://cs10308.userapi.com/u4718705/e_be62b8f2.jpg" />' +
+            '<div class="photo" title="' + viewer.name + '">' +
+                '<img src="' + viewer.photo + '" />' +
             '</div>' +
             '<div class="textarea-wrap">' +
                 '<textarea placeholder="Ответить..."></textarea>' +
@@ -82,15 +173,22 @@ Notification.prototype.makeMessageNotification = function() {
     '';
 
     var textarea = t.el.childNodes[1].childNodes[1].childNodes[0];
-    var history = t.el.childNodes[0];
     var closeButton = t.el.childNodes[2];
+
+    addEvent(window, 'focus', function() {
+        var text = trim(textarea.value);
+        if (!text && document.activeElement != textarea) {
+            t.setHideTimeout();
+        }
+    });
+    addEvent(window, 'blur', function() {
+        t.clearHideTimeout();
+    });
     addEvent(textarea, 'focus', function() {
-        t.isFocused = true;
         addClass(t.el, 'active');
         t.clearHideTimeout();
     });
     addEvent(textarea, 'blur', function() {
-        t.isFocused = false;
         var text = trim(textarea.value);
         if (!text) {
             removeClass(t.el, 'active');
@@ -105,29 +203,19 @@ Notification.prototype.makeMessageNotification = function() {
     });
     addEvent(t.el, 'mouseout', function(e) {
         var text = trim(textarea.value);
-        if (!text && !t.isFocused) {
+        if (!text && document.activeElement != textarea) {
             t.setHideTimeout();
         }
     });
     addEvent(textarea, 'keyup', function(e) {
         var text = trim(textarea.value);
         if (e.keyCode == KEY.ENTER) {
-            var message = ce('div');
-            message.className = 'message';
-            message.innerHTML =
-                '<div class="photo">' +
-                    '<img src="http://cs10308.userapi.com/u4718705/e_be62b8f2.jpg" />' +
-                '</div>' +
-                '<div class="text">' +
-                    text.replace(/\n/g, '<br>') +
-                '</div>' +
-            '';
-            history.appendChild(message);
-            message.style.marginBottom = -40 + 'px';
             textarea.value = '';
-            setTimeout(function() {
-                message.style.marginBottom = 0;
-            }, 0);
+            var message = new Message({
+                text: text,
+                user: viewer
+            });
+            t.addMessage(message)
         } else if (e.keyCode == KEY.ESC) {
             t.hide();
         }
@@ -139,13 +227,39 @@ Notification.prototype.makeMessageNotification = function() {
     t.setHideTimeout();
 };
 
+Notification.prototype.addMessage = function(message) {
+    var t = this;
+    var history = t.el.childNodes[0];
+    var messageElement = ce('div');
+    messageElement.className = 'message';
+    messageElement.innerHTML =
+        '<div class="photo" title="' + message.user.name + '">' +
+            '<img src="' + message.user.photo + '" />' +
+        '</div>' +
+        '<div class="text">' +
+            message.text.replace(/\n/g, '<br>') +
+        '</div>' +
+    '';
+    history.appendChild(messageElement);
+    messageElement.style.marginBottom = -40 + 'px';
+    setTimeout(function() {
+        messageElement.style.marginBottom = 0;
+    }, 0);
+};
+
 Notification.prototype.makeLikeNotification = function() {
     var t = this;
+    var user = t.options.user;
+
+    if (!(user instanceof User)) {
+        throw new TypeError('Invalid user');
+    }
+
     t.el.innerHTML =
         '<div class="history">' +
             '<div class="message">' +
                 '<div class="photo">' +
-                    '<img src="http://cs10308.userapi.com/u4718705/e_be62b8f2.jpg" />' +
+                    '<img src="' + user.photo + '" />' +
                 '</div>' +
                 '<div class="text">' +
                     'Вам постравили лайк' +
@@ -184,7 +298,6 @@ Notification.prototype.remove = function() {
     var t = this;
     Notification.el.removeChild(t.el);
     delete Notification.collection[t.id];
-    t = null;
 };
 
 Notification.prototype.clearHideTimeout = function() {
