dbf2b37ab4607bd4d21cce63f8764588b63f8189
Artyom Kohver <tema@kohver.ru>
2013-01-26 17:49:06 +0400

    Рефакторинг с использованием class.js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,36 @@
 /**
  * Модель всех пользователей
  * @param options
- * @constructor
  */
-var User = function(options) {
-    var t = this;
-    t.id = options.id + '';
-    t.name = options.name + '';
-    t.photo = options.photo + '';
-};
+var User = Class.extend({
+    init: function(options) {
+        var t = this;
+        t.id = options.id + '';
+        t.name = options.name + '';
+        t.photo = options.photo + '';
+    }
+});
 
 /**
  * Модель всех сообщений
  * @param options
- * @constructor
  */
-var Message = function(options) {
-    var t = this;
+var Message = Class.extend({
+    init: function(options) {
+        var t = this;
 
-    if (typeof options != 'object') {
-        throw new TypeError('Invalid options');
+        if (typeof options != 'object') {
+            throw new TypeError('Invalid options');
+        }
+
+        if (!(options.user instanceof User)) {
+            throw new TypeError('Invalid user');
+        }
+
+        t.text = options.text + '';
+        t.user = options.user;
     }
-
-    if (!(options.user instanceof User)) {
-        throw new TypeError('Invalid user');
-    }
-
-    t.text = options.text + '';
-    t.user = options.user;
-};
+});
 
 (function() {
     window.viewer = new User({
@@ -57,7 +59,7 @@ var Message = function(options) {
         sendNotification();
     });
     addEvent('like-button', 'click', function() {
-        new Notification(Notification.LIKE, {
+        new LikeNotification({
             user: defaultUser
         });
     });
@@ -83,12 +85,13 @@ var Message = function(options) {
             textarea.value = '';
             if (lastMessageNotification &&
                 !lastMessageNotification.isHidden &&
-                message.user.id == lastMessageNotification.options.message.user.id) {
+                message.user.id == lastMessageNotification.options.message.user.id)
+            {
                 lastMessageNotification.addMessage(message);
                 lastMessageNotification.clearHideTimeout();
                 lastMessageNotification.setHideTimeout();
             } else {
-                lastMessageNotification = new Notification(Notification.MESSAGE, {
+                lastMessageNotification = new MessageNotification({
                     message: message
                 });
             }
@@ -100,227 +103,232 @@ var Message = function(options) {
 })();
 
 /**
- * Класс оповещений
- * @param type
- * @constructor
+ * Базовый класс оповещений
+ * @class Notification
+ * @extends Class
  */
-var Notification = function(type) {
-    var t = this;
+var Notification = Class.extend({
+    init: function(options) {
+        var t = this;
 
-    t.id = ++Notification.lastId;
-    t.type = type;
-    t.options = arguments[1] || {};
-    t.isHidden = true;
-    t.el = ce('div');
-    t.el.className = Notification.className;
-    t.el.style.zIndex = t.id;
+        t.id = ++Notification.lastId;
+        t.options = options || {};
+        t.isHidden = true;
+        t.el = ce('div');
+        t.el.className = Notification.className;
+        t.el.style.zIndex = t.id;
 
-    Notification.collection[t.id] = t;
-    t.show();
-};
+        Notification.collection[t.id] = t;
+        t.show();
+    },
 
-Notification.prototype.show = function() {
-    var t = this;
-    t.isHidden = false;
+    show: function() {
+        var t = this;
+        Notification.el.appendChild(t.el);
+        t.isHidden = false;
+        t.makeElement();
 
-    Notification.el.appendChild(t.el);
+        setTimeout(function() {
+            t.el.style.marginBottom = 0;
+            t.el.style.opacity = 1;
+        }, 0);
+    },
 
-    switch(t.type) {
-        case Notification.MESSAGE:
-            t.makeMessageNotification();
-            break;
-        case Notification.LIKE:
-            t.makeLikeNotification();
-            break;
-        default:
-            return;
-    }
+    hide: function() {
+        var t = this;
+        t.isHidden = true;
+        t.el.style.opacity = 0;
+        t.el.style.marginBottom = -getHeight(t.el) - 10 + 'px';
+        setTimeout(function() {
+            t.remove();
+        }, 200);
+    },
 
-    setTimeout(function() {
-        t.el.style.marginBottom = 0;
-        t.el.style.opacity = 1;
-    }, 0);
-};
+    remove: function() {
+        var t = this;
+        Notification.el.removeChild(t.el);
+        delete Notification.collection[t.id];
+    },
 
-Notification.prototype.makeMessageNotification = function() {
-    var t = this;
-    var message = t.options.message;
+    clearHideTimeout: function() {
+        var t = this;
+        clearTimeout(t.timeout);
+    },
 
-    if (!(message instanceof Message)) {
-        throw new TypeError('Invalid message');
-    }
+    setHideTimeout: function() {
+        var t = this;
+        t.timeout = setTimeout(function() {
+            if (!t.isHidden) {
+                t.hide();
+            }
+        }, Notification.delay);
+    },
 
-    t.el.innerHTML =
-        '<div class="history">' +
-            '<div class="message">' +
-                '<div class="photo" title="' + message.user.name + '">' +
-                    '<img src="' + message.user.photo + '" />' +
-                '</div>' +
-                '<div class="text">' +
-                    message.text.replace(/\n/g, '<br>') +
-                '</div>' +
-            '</div>' +
-        '</div>' +
-        '<div class="textarea-bg">' +
-            '<div class="photo" title="' + viewer.name + '">' +
-                '<img src="' + viewer.photo + '" />' +
-            '</div>' +
-            '<div class="textarea-wrap">' +
-                '<textarea placeholder="Ответить..."></textarea>' +
-            '</div>' +
-        '</div>' +
-        '<div class="close"></div>' +
-    '';
-
-    var textarea = t.el.childNodes[1].childNodes[1].childNodes[0];
-    var closeButton = t.el.childNodes[2];
-
-    addEvent(window, 'focus', function() {
-        var text = trim(textarea.value);
-        if (!text && document.activeElement != textarea) {
-            t.setHideTimeout();
-        }
-    });
-    addEvent(window, 'blur', function() {
-        t.clearHideTimeout();
-    });
-    addEvent(textarea, 'focus', function() {
-        addClass(t.el, 'active');
-        t.clearHideTimeout();
-    });
-    addEvent(textarea, 'blur', function() {
-        var text = trim(textarea.value);
-        if (!text) {
-            removeClass(t.el, 'active');
-            t.setHideTimeout();
-        }
-    });
-    addEvent(t.el, 'click', function() {
-        textarea.focus();
-    });
-    addEvent(t.el, 'mouseover', function(e) {
-        t.clearHideTimeout();
-    });
-    addEvent(t.el, 'mouseout', function(e) {
-        var text = trim(textarea.value);
-        if (!text && document.activeElement != textarea) {
-            t.setHideTimeout();
-        }
-    });
-    addEvent(textarea, 'keyup', function(e) {
-        var text = trim(textarea.value);
-        if (e.keyCode == KEY.ENTER) {
-            textarea.value = '';
-            var message = new Message({
-                text: text,
-                user: viewer
-            });
-            t.addMessage(message)
-        } else if (e.keyCode == KEY.ESC) {
-            t.hide();
-        }
-    });
-    addEvent(closeButton, 'mousedown', function(e) {
-        t.hide();
-        e.stopPropagation();
-    });
-    t.setHideTimeout();
-};
-
-Notification.prototype.addMessage = function(message) {
-    var t = this;
-    var history = t.el.childNodes[0];
-    var messageElement = ce('div');
-    messageElement.className = 'message';
-    messageElement.innerHTML =
-        '<div class="photo" title="' + message.user.name + '">' +
-            '<img src="' + message.user.photo + '" />' +
-        '</div>' +
-        '<div class="text">' +
-            message.text.replace(/\n/g, '<br>') +
-        '</div>' +
-    '';
-    history.appendChild(messageElement);
-    messageElement.style.marginBottom = -40 + 'px';
-    setTimeout(function() {
-        messageElement.style.marginBottom = 0;
-    }, 0);
-};
-
-Notification.prototype.makeLikeNotification = function() {
-    var t = this;
-    var user = t.options.user;
-
-    if (!(user instanceof User)) {
-        throw new TypeError('Invalid user');
-    }
-
-    t.el.innerHTML =
-        '<div class="history">' +
-            '<div class="message">' +
-                '<div class="photo">' +
-                    '<img src="' + user.photo + '" />' +
-                '</div>' +
-                '<div class="text">' +
-                    'Вам постравили лайк' +
-                '</div>' +
-            '</div>' +
-        '</div>' +
-        '<div class="close"></div>' +
-    '';
-
-    var closeButton = t.el.childNodes[1];
-    addClass(t.el, 'like');
-    addEvent(t.el, 'mouseover', function(e) {
-        t.clearHideTimeout();
-    });
-    addEvent(t.el, 'mouseout', function(e) {
-        t.setHideTimeout();
-    });
-    addEvent(closeButton, 'mousedown', function(e) {
-        t.hide();
-        e.stopPropagation();
-    });
-    t.setHideTimeout();
-};
-
-Notification.prototype.hide = function() {
-    var t = this;
-    t.isHidden = true;
-    t.el.style.opacity = 0;
-    t.el.style.marginBottom = -getHeight(t.el) - 10 + 'px';
-    setTimeout(function() {
-        t.remove();
-    }, 200);
-};
-
-Notification.prototype.remove = function() {
-    var t = this;
-    Notification.el.removeChild(t.el);
-    delete Notification.collection[t.id];
-};
-
-Notification.prototype.clearHideTimeout = function() {
-    var t = this;
-    clearTimeout(t.timeout);
-};
-
-Notification.prototype.setHideTimeout = function() {
-    var t = this;
-    t.timeout = setTimeout(function() {
-        if (!t.isHidden) {
-            t.hide();
-        }
-    }, Notification.delay);
-};
+    makeElement: function() {},
+    addMessage: function() {}
+});
 
 Notification.lastId = 0;
 Notification.collection = {};
 Notification.el = ge('notifications');
 Notification.className = 'notification';
-Notification.MESSAGE = 'message';
-Notification.LIKE = 'like';
 Notification.delay = 5000;
 Notification.count = function() {
     return countObj(Notification.collection);
 };
+
+/**
+ * Класс оповещений новых сообщений
+ * @class MessageNotification
+ * @extends Notification
+ */
+var MessageNotification = Notification.extend({
+    makeElement: function() {
+        var t = this;
+        var message = t.options.message;
+
+        if (!(message instanceof Message)) {
+            throw new TypeError('Invalid message');
+        }
+
+        t.el.innerHTML =
+            '<div class="history">' +
+                '<div class="message">' +
+                    '<div class="photo" title="' + message.user.name + '">' +
+                        '<img src="' + message.user.photo + '" />' +
+                    '</div>' +
+                    '<div class="text">' +
+                        message.text.replace(/\n/g, '<br>') +
+                    '</div>' +
+                '</div>' +
+            '</div>' +
+            '<div class="textarea-bg">' +
+                '<div class="photo" title="' + viewer.name + '">' +
+                    '<img src="' + viewer.photo + '" />' +
+                '</div>' +
+                '<div class="textarea-wrap">' +
+                    '<textarea placeholder="Ответить..."></textarea>' +
+                '</div>' +
+            '</div>' +
+            '<div class="close"></div>' +
+            '';
+
+        var textarea = t.el.childNodes[1].childNodes[1].childNodes[0];
+        var closeButton = t.el.childNodes[2];
+
+        addEvent(window, 'focus', function() {
+            var text = trim(textarea.value);
+            if (!text && document.activeElement != textarea) {
+                t.setHideTimeout();
+            }
+        });
+        addEvent(window, 'blur', function() {
+            t.clearHideTimeout();
+        });
+        addEvent(textarea, 'focus', function() {
+            addClass(t.el, 'active');
+            t.clearHideTimeout();
+        });
+        addEvent(textarea, 'blur', function() {
+            var text = trim(textarea.value);
+            if (!text) {
+                removeClass(t.el, 'active');
+                t.setHideTimeout();
+            }
+        });
+        addEvent(t.el, 'click', function() {
+            textarea.focus();
+        });
+        addEvent(t.el, 'mouseover', function(e) {
+            t.clearHideTimeout();
+        });
+        addEvent(t.el, 'mouseout', function(e) {
+            var text = trim(textarea.value);
+            if (!text && document.activeElement != textarea) {
+                t.setHideTimeout();
+            }
+        });
+        addEvent(textarea, 'keyup', function(e) {
+            var text = trim(textarea.value);
+            if (e.keyCode == KEY.ENTER) {
+                textarea.value = '';
+                var message = new Message({
+                    text: text,
+                    user: viewer
+                });
+                t.addMessage(message)
+            } else if (e.keyCode == KEY.ESC) {
+                t.hide();
+            }
+        });
+        addEvent(closeButton, 'mousedown', function(e) {
+            t.hide();
+            e.stopPropagation();
+        });
+        t.setHideTimeout();
+    },
+
+    addMessage: function(message) {
+        var t = this;
+        var history = t.el.childNodes[0];
+        var messageElement = ce('div');
+        messageElement.className = 'message';
+        messageElement.innerHTML =
+            '<div class="photo" title="' + message.user.name + '">' +
+                '<img src="' + message.user.photo + '" />' +
+            '</div>' +
+            '<div class="text">' +
+                message.text.replace(/\n/g, '<br>') +
+            '</div>' +
+            '';
+        history.appendChild(messageElement);
+        messageElement.style.marginBottom = -40 + 'px';
+        setTimeout(function() {
+            messageElement.style.marginBottom = 0;
+        }, 0);
+    }
+});
+
+/**
+ * Класс оповещений Мне нравится
+ * @class LikeNotification
+ * @extends Notification
+ */
+var LikeNotification = Notification.extend({
+    makeElement: function() {
+        var t = this;
+        var user = t.options.user;
+
+        if (!(user instanceof User)) {
+            throw new TypeError('Invalid user');
+        }
+
+        t.el.innerHTML =
+            '<div class="history">' +
+                '<div class="message">' +
+                    '<div class="photo">' +
+                        '<img src="' + user.photo + '" />' +
+                    '</div>' +
+                    '<div class="text">' +
+                        'Вам постравили лайк' +
+                    '</div>' +
+                '</div>' +
+            '</div>' +
+            '<div class="close"></div>' +
+            '';
+
+        var closeButton = t.el.childNodes[1];
+        addClass(t.el, 'like');
+        addEvent(t.el, 'mouseover', function(e) {
+            t.clearHideTimeout();
+        });
+        addEvent(t.el, 'mouseout', function(e) {
+            t.setHideTimeout();
+        });
+        addEvent(closeButton, 'mousedown', function(e) {
+            t.hide();
+            e.stopPropagation();
+        });
+        t.setHideTimeout();
+    }
+});
